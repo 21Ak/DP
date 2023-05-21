a2c179e3bad22f765276caab27fcd14e6c540ad3
Akash_Ktr <akash.katiyar.met21@itbhu.ac.in>
2023-05-21 11:24:18 +0530

    Slight Change

--- DP on Subsequences/SubsequenceSumK.cpp
+++ DP on Subsequences/SubsequenceSumK.cpp
@@ -19,7 +19,7 @@ bool subsetSumToK(int n, int k, vector<int> &arr) {
     // TABULATION
     vector<bool> prev(k+1, 0), cur(k+1, 0);
     prev[0] = cur[0] = true;
-    prev[arr[0]] = true;
+    if(arr[0]<=k)   prev[arr[0]] = true;
 
     for(int ind = 1; ind<=n-1; ind++){
         for(int target = 1; target<= k; target++){
